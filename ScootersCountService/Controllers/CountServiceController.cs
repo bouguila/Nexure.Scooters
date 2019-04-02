561153807290e23dff4fea41b3bd22fb89a2802b
bouguila <jawher@starcounter.com>
2019-04-03 01:09:25 +0200

    init commit

---
+++ ScootersCountService/Controllers/CountServiceController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ScootersCountService.Core;
+
+namespace ScootersCountService.Controllers
+{
+    [Route("/countService")]
+    [ApiController]
+    public class CountServiceController : ControllerBase
+    {
+        private ICountService _countService;
+        public CountServiceController(ICountService countService)
+        {
+            this._countService = countService;
+        }
+        // POST /countService
+        [HttpPost]
+        public ActionResult<int> Post([FromBody] RequestBody body)
+        {
+            int[] scooters = body.Scooters;
+            int c = body.C;
+            int p = body.P;
+
+            var result = _countService.CountMinFE(scooters, c, p);
+            return result;
+        }
+    }
+
+    public class RequestBody
+    {
+        public int[] Scooters { get; set; }
+        public int C { get; set; }
+        public int P { get; set; }
+    }
+}
---
+++ ScootersCountService/Core/CountService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScootersCountService.Core
+{
+    public class CountService : ICountService
+    {
+        public int CountMinFE(int[] scooters, int c, int p)
+        {
+            int totalFEs = 0;
+            int remainFE = 0;
+
+            foreach (int scooter in scooters)
+            {
+                int requiredFE = scooter / p + ((scooter % p == 0) ? 0 : 1);
+                totalFEs += requiredFE;
+
+                int remainingScooters = Math.Max(scooter - c, 0);
+                int FEsupervise = remainingScooters / p + ((remainingScooters % p == 0) ? 0 : 1);
+                int minimumFEs = requiredFE - FEsupervise;
+                remainFE = minimumFEs > remainFE ? minimumFEs : remainFE;
+            }
+
+            return totalFEs - remainFE;
+        }
+    }
+}
